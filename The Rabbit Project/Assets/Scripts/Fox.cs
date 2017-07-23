1e317fc310f737e9d10f6c8c075b8a10ec1fca5a
Gruppe1 <Gr1@gdc.dk>
2017-07-23 21:52:05 +0200

    Her er alt hvad Vanilla og Marius har lavet på første dag...

---
+++ The Rabbit Project/Assets/Scripts/Fox.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fox : MonoBehaviour {
+
+    public float Movement = 5;
+    public Rigidbody myRigidbody;
+
+
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Move(Movement * Input.GetAxis("Horizontal"));
+    }
+
+    public void Move(float speed)
+    {
+        myRigidbody.velocity = transform.forward * speed + Vector3.up * myRigidbody.velocity.y;
+
+    }
+}
