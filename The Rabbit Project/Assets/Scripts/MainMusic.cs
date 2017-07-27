6f01c9aec1c7789c6a6ced964fa76b7083441b47
Gruppe1 <Gr1@gdc.dk>
2017-07-28 00:53:39 +0200

    GAME DONE -EEEEEEEEEEEEEIIIIIHHHHHH

---
+++ The Rabbit Project/Assets/Scripts/MainMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (SceneManager.GetSceneAt (level).name == "DEATH" || SceneManager.GetSceneAt(level).name == "Menu")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
